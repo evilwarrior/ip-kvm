c2315fd172e89e0b1a5c05b3d912888cca8da599
evilwarrior <evilwarriorex@gmail.com>
2022-10-08 17:28:43 +0800

    add original front panel buttons support

--- kvm.ino
+++ kvm.ino
@@ -3,8 +3,10 @@
  *  ATX Power Supply Controlling
  */
 #include <Keyboard.h>
-#define PWR 6           // PWR button signal
-#define RST 9           // RST button signal
+#define PWR 6           // PWR button emulator pin
+#define RST 9           // RST button emulator pin
+#define ORI_PWR   2     // Original power button pin
+#define ORI_RST   21    // Original reset button pin
 
 #define PSC_FIELD 0xfd  // Command used for power supply controlling
 #define KEY_RLS   0x00  // Key release command
@@ -13,14 +15,19 @@
 #define RST_S     0xfe  // Short press reset button command
 #define PWR_L     0xff  // Long press power button command
 
-bool bPress = true;
+bool bPress = true;     // Is KeyBoard pressed?
 
 void setup()
 {
+    // I/O Setup
     digitalWrite(PWR, LOW);
     digitalWrite(RST, LOW);
     pinMode(PWR, OUTPUT);
     pinMode(RST, OUTPUT);
+    pinMode(ORI_PWR, INPUT);
+    pinMode(ORI_RST, INPUT);
+    digitalWrite(ORI_PWR, HIGH);
+    digitalWrite(ORI_RST, HIGH);
     Serial1.begin(9600);  // Init hardware serial port
     Keyboard.begin();     // Init keyboard emulation
 }
@@ -41,7 +48,7 @@ void loop()
             {
                 bPress = true;
             }
-            else                    // Emulate keyboard press/release
+            else                       // Emulate keyboard press/release
             {
                 if (bPress)
                 {
@@ -75,5 +82,19 @@ void loop()
                 digitalWrite(PWR, LOW);
             }
         }
+    }
+
+    // Transform original power button input into power button emulator output
+    int nSt = digitalRead(PWR);
+    if (digitalRead(ORI_PWR) == nSt)
+    {
+        digitalWrite(PWR, nSt^0x01);
+    }
+
+    // Transform original reset button input into reset button emulator output
+    nSt = digitalRead(RST);
+    if (digitalRead(ORI_RST) == nSt)
+    {
+        digitalWrite(RST, nSt^0x01);
     }
 }
